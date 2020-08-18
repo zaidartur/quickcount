9fafb7dddb6af24567410444a55e705c21eabb37
Zaid Artur <zaidartur@gmail.com>
2020-08-18 09:47:27 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+//set timezone
+date_default_timezone_set('Asia/Jakarta');
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
