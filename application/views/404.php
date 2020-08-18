192ee802b0d9b1624b9a13b42944cdf63640109c
Zaid Artur <zaidartur@gmail.com>
2020-08-18 09:48:18 +0700

    Create 404.php

---
+++ application/views/404.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Page Not Found!</title>
+    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 11]>
+    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="">
+    <meta name="author" content="Phoenixcoded" />
+
+    <!-- Favicon icon -->
+    <link rel="icon" href="assets/images/favicon.svg" type="image/x-icon">
+
+    <!-- font css -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/fonts/font-awsome-pro/css/pro.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/fonts/feather.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/fonts/fontawesome.css">
+
+    <!-- vendor css -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/customizer.css">
+
+</head>
+<!-- [ offline-ui ] start -->
+<div class="auth-wrapper maintance">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="text-center">
+                    <img src="<?php echo base_url() ?>assets/images/404.png" alt="" class="img-fluid">
+                    <h5 class="text-muted my-4">Oops! Page not found!</h5>
+                    <form action="<?php echo base_url() ?>">
+                        <button class="btn waves-effect waves-light btn-primary mb-4"><i class="feather icon-home mr-2"></i>Back to Home</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- [ offline-ui ] end -->
+<!-- Required Js -->
+<script src="<?php echo base_url() ?>assets/js/vendor-all.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/plugins/bootstrap.min.js"></script>
+
+<script>
+    // header option
+    $('#pct-toggler').on('click', function() {
+        $('.pct-customizer').toggleClass('active');
+
+    });
+    // header option
+    $('#cust-sidebrand').change(function() {
+        if ($(this).is(":checked")) {
+            $('.theme-color.brand-color').removeClass('d-none');
+            $('.m-header').addClass('bg-dark');
+        } else {
+            $('.m-header').removeClassPrefix('bg-');
+            $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo-dark.svg');
+            $('.theme-color.brand-color').addClass('d-none');
+        }
+    });
+    // Header Color
+    $('.brand-color > a').on('click', function() {
+        var temp = $(this).attr('data-value');
+        // $('.header-color > a').removeClass('active');
+        // $('.pcoded-header').removeClassPrefix('brand-');
+        // $(this).addClass('active');
+        if (temp == "bg-default") {
+            $('.m-header').removeClassPrefix('bg-');
+        } else {
+            $('.m-header').removeClassPrefix('bg-');
+            $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo.svg');
+            $('.m-header').addClass(temp);
+        }
+    });
+    // Header Color
+    $('.header-color > a').on('click', function() {
+        var temp = $(this).attr('data-value');
+        // $('.header-color > a').removeClass('active');
+        // $('.pcoded-header').removeClassPrefix('brand-');
+        // $(this).addClass('active');
+        if (temp == "bg-default") {
+            $('.pc-header').removeClassPrefix('bg-');
+        } else {
+            $('.pc-header').removeClassPrefix('bg-');
+            $('.pc-header').addClass(temp);
+        }
+    });
+    // sidebar option
+    $('#cust-sidebar').change(function() {
+        if ($(this).is(":checked")) {
+            $('.pc-sidebar').addClass('light-sidebar');
+            $('.pc-horizontal .topbar').addClass('light-sidebar');
+            // $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo-dark.svg');
+        } else {
+            $('.pc-sidebar').removeClass('light-sidebar');
+            $('.pc-horizontal .topbar').removeClass('light-sidebar');
+            // $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo.svg');
+        }
+    });
+    $.fn.removeClassPrefix = function(prefix) {
+        this.each(function(i, it) {
+            var classes = it.className.split(" ").map(function(item) {
+                return item.indexOf(prefix) === 0 ? "" : item;
+            });
+            it.className = classes.join(" ");
+        });
+        return this;
+    };
+</script>
+
+</body>
+</html>
