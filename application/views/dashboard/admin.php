e11d468ea2c826e240803589a59d541960b8ae50
Zaid Artur <zaidartur@gmail.com>
2020-08-18 09:48:16 +0700

    Create admin.php

---
+++ application/views/dashboard/admin.php
@@ -0,0 +1,63 @@
+            <!-- [ Main Content ] start -->
+            <div class="row">
+                <!-- visitors  start -->
+                <!-- <div class="col-sm-3">
+                    <div class="card bg-primary text-white widget-visitor-card">
+                        <div class="card-body text-center">
+                            <h2 class="text-white">18</h2>
+                            <h6 class="text-white">Registered Users</h6>
+                            <i class="feather icon-user-plus"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card bg-success text-white widget-visitor-card">
+                        <div class="card-body text-center">
+                            <h2 class="text-white">14</h2>
+                            <h6 class="text-white">Active Users</h6>
+                            <i class="feather icon-user-check"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card bg-danger text-white widget-visitor-card">
+                        <div class="card-body text-center">
+                            <h2 class="text-white">1</h2>
+                            <h6 class="text-white">Pending Users</h6>
+                            <i class="feather icon-user-minus"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card bg-warning text-white widget-visitor-card">
+                        <div class="card-body text-center">
+                            <h2 class="text-white">16</h2>
+                            <h6 class="text-white">Active Memberships</h6>
+                            <i class="feather icon-users"></i>
+                        </div>
+                    </div>
+                </div> -->
+                <!-- visitors  end -->
+
+                <!-- progressbar static data start -->
+                <div class="col-md-12">
+                    <!-- <div class="card">
+                        <div class="card-header">
+                            <h5>Bar Chart</h5>
+                        </div>
+                        <div class="card-body"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                            <canvas id="chart-bar-1" style="width: 450px; height: 300px; display: block;" width="450" height="300" class="chartjs-render-monitor"></canvas>
+                        </div>
+                    </div> -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>Website Audience Metrics</h5>
+                        </div>
+                        <div class="card-body pl-0 pb-0">
+                            <div id="revenue-chart"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- progressbar static data end -->
+            </div>
+            <!-- [ Main Content ] end -->
