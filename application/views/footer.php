2955834809e4e8ffe644294dc6c0b140273f16f8
Zaid Artur <zaidartur@gmail.com>
2020-08-18 09:48:13 +0700

    Create footer.php

---
+++ application/views/footer.php
@@ -0,0 +1,400 @@
+        </div>
+    </div>
+    <!-- [ Main Content ] end -->
+
+
+    <!-- Warning Section start -->
+    <!-- Older IE warning message -->
+    <!--[if lt IE 11]>
+        <div class="ie-warning">
+            <h1>Warning!!</h1>
+            <p>You are using an outdated version of Internet Explorer, please upgrade
+               <br/>to any of the following web browsers to access this website.
+            </p>
+            <div class="iew-container">
+                <ul class="iew-download">
+                    <li>
+                        <a href="http://www.google.com/chrome/">
+                            <img src="assets/images/browser/chrome.png" alt="Chrome">
+                            <div>Chrome</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.mozilla.org/en-US/firefox/new/">
+                            <img src="assets/images/browser/firefox.png" alt="Firefox">
+                            <div>Firefox</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.opera.com">
+                            <img src="assets/images/browser/opera.png" alt="Opera">
+                            <div>Opera</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.apple.com/safari/">
+                            <img src="assets/images/browser/safari.png" alt="Safari">
+                            <div>Safari</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                            <img src="assets/images/browser/ie.png" alt="">
+                            <div>IE (11 & above)</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <p>Sorry for the inconvenience!</p>
+        </div>
+    <![endif]-->
+    <!-- Warning Section Ends -->
+
+
+    <!-- Required Js -->
+    <script src="<?php echo base_url()?>assets/js/vendor-all.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/plugins/bootstrap.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/plugins/feather.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/pcoded.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/plugins/clipboard.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/uikit.min.js"></script>
+
+    <script src="<?php echo base_url()?>assets/js/plugins/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/plugins/dataTables.bootstrap4.min.js"></script>
+
+    <!-- chartjs js -->
+    <script src="<?php echo base_url()?>assets/js/plugins/Chart.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/pages/chart-chart-custom.js"></script>
+
+    <!-- Apex Chart -->
+    <script src="<?php echo base_url()?>assets/js/plugins/apexcharts.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/pages/todo.js"></script>
+
+    <script>
+        // [ revenue-map ] start
+        $(function() {
+            var options = {
+                chart: {
+                    height: 200,
+                    type: 'line',
+                    toolbar: {
+                        show: false,
+                    },
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    width: 3,
+                    curve: 'smooth'
+                },
+                series: [{
+                    name: 'Sales',
+                    data: [20, 50, 30, 60, 30, 50, 40]
+                }, {
+                    name: 'Amount',
+                    data: [40, 20, 50, 15, 40, 65, 20]
+                }],
+                xaxis: {
+                    type: 'datetime',
+                    categories: ['1/11/2019', '2/11/2019', '3/11/2019', '4/11/2019', '5/11/2019', '6/11/2019', '7/11/2019'],
+                },
+                colors: ['#448aff', '#17C666'],
+                fill: {
+                    type: 'solid',
+                },
+                markers: {
+                    size: 5,
+                    colors: ['#448aff', '#17C666'],
+                    opacity: 0.9,
+                    strokeWidth: 2,
+                    hover: {
+                        size: 7,
+                    }
+                },
+                grid: {
+                    borderColor: '#e2e5e885',
+                },
+                yaxis: {
+                    min: 10,
+                    max: 70,
+                }
+            };
+            var chart = new ApexCharts(document.querySelector("#collected-chart"), options);
+            chart.render();
+        });
+        // [ revenue-map ] end
+        // [ sales-chart ] start
+        $(function() {
+            var options = {
+                chart: {
+                    height: 250,
+                    type: 'line',
+                    toolbar: {
+                        show: false,
+                    },
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    width: 2,
+                    curve: 'smooth'
+                },
+                series: [{
+                    name: 'Trial',
+                    data: [20, 50, 30, 60, 30, 50, 40]
+                }, {
+                    name: 'Bronze',
+                    data: [40, 20, 50, 15, 40, 65, 20]
+                }, {
+                    name: 'Gold',
+                    data: [64, 40, 20, 30, 20, 40, 65]
+                }, {
+                    name: 'Platinum',
+                    data: [30, 25, 40, 15, 20, 15, 30]
+                }],
+                xaxis: {
+                    type: 'datetime',
+                    categories: ['1/11/2019', '2/11/2019', '3/11/2019', '4/11/2019', '5/11/2019', '6/11/2019', '7/11/2019'],
+                },
+                colors: ['#7267EF', '#17C666', "#ffba57", "#EA4D4D"],
+                fill: {
+                    type: 'solid',
+                },
+                markers: {
+                    size: 5,
+                    colors: ['#7267EF', '#17C666', "#ffba57", "#EA4D4D"],
+                    opacity: 0.9,
+                    strokeWidth: 2,
+                    hover: {
+                        size: 7,
+                    }
+                },
+                grid: {
+                    borderColor: '#e2e5e885',
+                },
+                yaxis: {
+                    min: 10,
+                    max: 70,
+                }
+            };
+            var chart = new ApexCharts(document.querySelector("#sales-chart"), options);
+            chart.render();
+        });
+        // [ sales-chart ] end
+        // [ Transection ] start
+        $(function() {
+            var options1 = {
+                chart: {
+                    type: 'bar',
+                    height: 100,
+                    sparkline: {
+                        enabled: true
+                    }
+                },
+                colors: ["#7267EF"],
+                plotOptions: {
+                    bar: {
+                        columnWidth: '80%'
+                    }
+                },
+                series: [{
+                    data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+                }],
+                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                xaxis: {
+                    crosshairs: {
+                        width: 1
+                    },
+                },
+                tooltip: {
+                    fixed: {
+                        enabled: false
+                    },
+                    x: {
+                        show: false
+                    },
+                    y: {
+                        title: {
+                            formatter: function(seriesName) {
+                                return 'Amount'
+                            }
+                        }
+                    },
+                    marker: {
+                        show: false
+                    }
+                }
+            }
+            new ApexCharts(document.querySelector("#transactions1"), options1).render();
+            var options2 = {
+                chart: {
+                    type: 'bar',
+                    height: 100,
+                    sparkline: {
+                        enabled: true
+                    }
+                },
+                colors: ["#EA4D4D"],
+                plotOptions: {
+                    bar: {
+                        columnWidth: '80%'
+                    }
+                },
+                series: [{
+                    data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+                }],
+                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                xaxis: {
+                    crosshairs: {
+                        width: 1
+                    },
+                },
+                tooltip: {
+                    fixed: {
+                        enabled: false
+                    },
+                    x: {
+                        show: false
+                    },
+                    y: {
+                        title: {
+                            formatter: function(seriesName) {
+                                return 'Sales'
+                            }
+                        }
+                    },
+                    marker: {
+                        show: false
+                    }
+                }
+            }
+            new ApexCharts(document.querySelector("#transactions2"), options2).render();
+        });
+        // [ Transection ] end
+    </script>
+
+    <!-- floating button -->
+    <div class="pct-customizer">
+        <div href="#!" class="pct-c-btn">
+            <button class="btn btn-light-danger" id="pct-toggler" data-toggle="tooltip" title="Setting" data-placement="left">
+                <i data-feather="settings"></i>
+            </button>
+            <button class="btn btn-light-primary" data-toggle="tooltip" title="Info" data-placement="left">
+                <i data-feather="info"></i>
+            </button>
+        </div>
+        <div class="pct-c-content ">
+            <div class="pct-header bg-primary">
+                <h5 class="mb-0 text-white f-w-500">Tampilan Sidebar & Header</h5>
+            </div>
+            <div class="pct-body">
+                <h6 class="mt-2"><i data-feather="credit-card" class="mr-2"></i>Header settings</h6>
+                <hr class="my-2">
+                <div class="theme-color header-color">
+                    <a href="#!" class="" data-value="bg-default"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-primary"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-danger"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-warning"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-info"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-success"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-dark"><span></span><span></span></a>
+                </div>
+                <h6 class="mt-4"><i data-feather="layout" class="mr-2"></i>Sidebar settings</h6>
+                <hr class="my-2">
+                <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="cust-sidebar">
+                    <label class="custom-control-label f-w-600 pl-1" for="cust-sidebar">Light Sidebar</label>
+                </div>
+                <div class="custom-control custom-switch mt-2">
+                    <input type="checkbox" class="custom-control-input" id="cust-sidebrand">
+                    <label class="custom-control-label f-w-600 pl-1" for="cust-sidebrand">Color Brand</label>
+                </div>
+                <div class="theme-color brand-color d-none">
+                    <a href="#!" class="active" data-value="bg-primary"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-danger"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-warning"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-info"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-success"><span></span><span></span></a>
+                    <a href="#!" class="" data-value="bg-dark"><span></span><span></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end of floating button -->
+
+    <script>
+        // header option
+        $('#pct-toggler').on('click', function() {
+            $('.pct-customizer').toggleClass('active');
+
+        });
+        // header option
+        $('#cust-sidebrand').change(function() {
+            if ($(this).is(":checked")) {
+                $('.theme-color.brand-color').removeClass('d-none');
+                $('.m-header').addClass('bg-dark');
+            } else {
+                $('.m-header').removeClassPrefix('bg-');
+                $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo-dark.svg');
+                $('.theme-color.brand-color').addClass('d-none');
+            }
+        });
+        // Header Color
+        $('.brand-color > a').on('click', function() {
+            var temp = $(this).attr('data-value');
+            // $('.header-color > a').removeClass('active');
+            // $('.pcoded-header').removeClassPrefix('brand-');
+            // $(this).addClass('active');
+            if (temp == "bg-default") {
+                $('.m-header').removeClassPrefix('bg-');
+            } else {
+                $('.m-header').removeClassPrefix('bg-');
+                $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo.svg');
+                $('.m-header').addClass(temp);
+            }
+        });
+        // Header Color
+        $('.header-color > a').on('click', function() {
+            var temp = $(this).attr('data-value');
+            // $('.header-color > a').removeClass('active');
+            // $('.pcoded-header').removeClassPrefix('brand-');
+            // $(this).addClass('active');
+            if (temp == "bg-default") {
+                $('.pc-header').removeClassPrefix('bg-');
+            } else {
+                $('.pc-header').removeClassPrefix('bg-');
+                $('.pc-header').addClass(temp);
+            }
+        });
+        // sidebar option
+        $('#cust-sidebar').change(function() {
+            if ($(this).is(":checked")) {
+                $('.pc-sidebar').addClass('light-sidebar');
+                $('.pc-horizontal .topbar').addClass('light-sidebar');
+                // $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo-dark.svg');
+            } else {
+                $('.pc-sidebar').removeClass('light-sidebar');
+                $('.pc-horizontal .topbar').removeClass('light-sidebar');
+                // $('.m-header > .b-brand > .logo-lg').attr('src', 'assets/images/logo.svg');
+            }
+        });
+        $.fn.removeClassPrefix = function(prefix) {
+            this.each(function(i, it) {
+                var classes = it.className.split(" ").map(function(item) {
+                    return item.indexOf(prefix) === 0 ? "" : item;
+                });
+                it.className = classes.join(" ");
+            });
+            return this;
+        };
+    </script>
+
+    <!-- custom-chart js -->
+    <script src="<?php echo base_url()?>assets/js/pages/dashboard-main.js"></script>
+
+</body>
+
+</html>
