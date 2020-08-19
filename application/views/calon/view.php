1cb23f6da495d7e0f307ed8cd65b2da35a4d017c
Zaid Artur <zaidartur@gmail.com>
2020-08-19 09:42:21 +0700

    Create view.php

---
+++ application/views/calon/view.php
@@ -0,0 +1,153 @@
+            <!-- [ Main Content ] start -->
+            <div class="row justify-content-center">
+        
+                <div class="col-sm-12">
+                    <div class="card">
+                        <!-- <div class="card-header">
+                            <h5>Data </h5>
+                        </div> -->
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-6 text-left">
+                                    <h5>Data Pasangan Calon</h5>    
+                                </div>
+                                <div class="col-sm-6 text-right">
+                                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal-report"><i class="feather icon-plus"></i>Tambah</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- progressbar static data end -->
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="card user-card user-card-1 mt-4">
+                        <div class="card-body pt-0">
+                            <div class="user-about-block text-center">
+                                <div class="row align-items-end">
+                                    <div class="col text-left pb-3">
+                                        <span class="badge badge-success">Active</span>
+                                    </div>
+                                    <div class="col"><img class="img-radius img-fluid wid-80" src="<?=base_url()?>assets/images/user/avatar-2.jpg" alt="User image"></div>
+                                    <div class="col text-right pb-3">
+                                        <div class="dropdown">
+                                            <a class="drp-icon dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="feather icon-more-horizontal"></i></a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" href="#">Edit</a>
+                                                <a class="dropdown-item" href="#">History</a>
+                                                <a class="dropdown-item" href="#">Trash</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <a href="#!" data-toggle="modal" data-target="#modal-report">
+                                    <h4 class="mb-1 mt-3">Andrew Burns</h4>
+                                </a>
+                                <p class="mb-3 text-muted"><i class="feather icon-calendar"> </i> 19 Jul 2016</p>
+                                <p class="mb-1"><b>Email : </b><a href="mailto:dummy@example.com">andrew.burns@example.com</a></p>
+                                <p class="mb-0"><b>Membership : </b>Platinum <span class="badge badge-warning">member</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card user-card user-card-1 mt-4">
+                        <div class="card-body pt-0">
+                            <div class="user-about-block text-center">
+                                <div class="row align-items-end">
+                                    <div class="col text-left pb-3">
+                                        <span class="badge badge-success">Active</span>
+                                    </div>
+                                    <div class="col"><img class="img-radius img-fluid wid-80" src="<?=base_url()?>assets/images/user/avatar-3.jpg" alt="User image"></div>
+                                    <div class="col text-right pb-3">
+                                        <div class="dropdown">
+                                            <a class="drp-icon dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="feather icon-more-horizontal"></i></a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" href="#">Edit</a>
+                                                <a class="dropdown-item" href="#">History</a>
+                                                <a class="dropdown-item" href="#">Trash</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <a href="#!" data-toggle="modal" data-target="#modal-report">
+                                    <h4 class="mb-1 mt-3">Keith Butler</h4>
+                                </a>
+                                <p class="mb-3 text-muted"><i class="feather icon-calendar"> </i> 27 Apr 2016</p>
+                                <p class="mb-1"><b>Email : </b><a href="mailto:dummy@example.com">Keith.butler@example.com</a></p>
+                                <p class="mb-0"><b>Membership : </b>TRIAL <span class="badge badge-warning">member</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card user-card user-card-1 mt-4">
+                        <div class="card-body pt-0">
+                            <div class="user-about-block text-center">
+                                <div class="row align-items-end">
+                                    <div class="col text-left pb-3">
+                                        <span class="badge badge-success">Active</span>
+                                    </div>
+                                    <div class="col"><img class="img-radius img-fluid wid-80" src="<?=base_url()?>assets/images/user/avatar-4.jpg" alt="User image"></div>
+                                    <div class="col text-right pb-3">
+                                        <div class="dropdown">
+                                            <a class="drp-icon dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="feather icon-more-horizontal"></i></a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" href="#">Edit</a>
+                                                <a class="dropdown-item" href="#">History</a>
+                                                <a class="dropdown-item" href="#">Trash</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <a href="#!" data-toggle="modal" data-target="#modal-report">
+                                    <h4 class="mb-1 mt-3">Christina Bowman</h4>
+                                </a>
+                                <p class="mb-3 text-muted"><i class="feather icon-calendar"> </i> 17 Apr 2016</p>
+                                <p class="mb-1"><b>Email : </b><a href="mailto:dummy@example.com">Christina.Bowman@example.com</a></p>
+                                <p class="mb-0"><b>Membership : </b>- <span class="badge badge-primary">staff</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card user-card user-card-1 mt-4">
+                        <div class="card-body pt-0">
+                            <div class="user-about-block text-center">
+                                <div class="row align-items-end">
+                                    <div class="col text-left pb-3">
+                                        <span class="badge badge-danger">Banned</span>
+                                    </div>
+                                    <div class="col"><img class="img-radius img-fluid wid-80" src="<?=base_url()?>assets/images/user/avatar-1.jpg" alt="User image"></div>
+                                    <div class="col text-right pb-3">
+                                        <div class="dropdown">
+                                            <a class="drp-icon dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="feather icon-more-horizontal"></i></a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" href="#">Edit</a>
+                                                <a class="dropdown-item" href="#">History</a>
+                                                <a class="dropdown-item" href="#">Trash</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <a href="#!" data-toggle="modal" data-target="#modal-report">
+                                    <h4 class="mb-1 mt-3">Kelly Montgomery</h4>
+                                </a>
+                                <p class="mb-3 text-muted"><i class="feather icon-calendar"> </i> 23 Mar 2016</p>
+                                <p class="mb-1"><b>Email : </b><a href="mailto:dummy@example.com">Kelly.Montgomery@example.com</a></p>
+                                <p class="mb-0"><b>Membership : </b>Bronze <span class="badge badge-warning">member</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- [ Main Content ] end -->
