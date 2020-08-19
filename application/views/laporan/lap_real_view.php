0cfc95a5696945171fa9fc625ebdfeac10e254d4
Zaid Artur <zaidartur@gmail.com>
2020-08-19 09:42:31 +0700

    Create lap_real_view.php

---
+++ application/views/laporan/lap_real_view.php
@@ -0,0 +1,16 @@
+            <!-- [ Main Content ] start -->
+            <div class="row">
+        
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>Data </h5>
+                        </div>
+                        <div class="card-body">
+                            <p>This is body.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- progressbar static data end -->
+            </div>
+            <!-- [ Main Content ] end -->
