5731b5cdecc6f8cfb292badf99d5ae9dcd1d6c8d
Zaid Artur <zaidartur@gmail.com>
2020-08-21 17:10:54 +0700

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -69,13 +69,60 @@
 
     <!-- notification Js -->
     <script src="<?php echo base_url()?>assets/js/plugins/bootstrap-notify.min.js"></script>
-    <script src="<?php echo base_url()?>assets/js/pages/ac-notification.js"></script>
 
     <!-- Apex Chart -->
     <script src="<?php echo base_url()?>assets/js/plugins/apexcharts.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/pages/todo.js"></script>
 
     <script>
+        <?php if($this->session->flashdata('sukses')) { ?>
+
+            $(window).on('load', function() {
+                function notify(message, title) {
+                    $.notify({
+                        icon: 'feather icon-check-circle',
+                        title: title,
+                        message: message
+                    }, {
+                        element: 'body',
+                        type: 'success',
+                        allow_dismiss: true,
+                        placement: {
+                            from: 'top',
+                            align: 'right'
+                        },
+                        delay: 2500,
+                        timer: 1000,
+                        spacing: 10,
+                        z_index: 999999,
+                        mouse_over: false,
+                        animate: {
+                            enter: 'animated bounceIn',
+                            exit: 'animated bounceOut'
+                        },
+                        offset: {
+                            x: 30,
+                            y: 30
+                        },
+                        icon_type: 'class',
+                        template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+                                    '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                                    '<span data-notify="icon"></span> ' +
+                                    '<span data-notify="title">{1}</span> <br>' +
+                                    '<span data-notify="message">{2}</span>' +
+                                    '<div class="progress" data-notify="progressbar">' +
+                                        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                                    '</div>' +
+                                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                                '</div>'
+                    });
+                };
+                notify('Data berhasil disimpan', 'Sukses');
+            });
+
+        <?php } ?>
+
+
         $('#user-list-table').DataTable();
         
         // [ revenue-map ] start
