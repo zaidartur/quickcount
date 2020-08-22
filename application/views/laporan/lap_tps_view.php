96e64527feb6775f9e6d425e8f4d341464a69c14
Zaid Artur <zaidartur@gmail.com>
2020-08-22 20:03:11 +0700

    Update lap_tps_view.php

--- application/views/laporan/lap_tps_view.php
+++ application/views/laporan/lap_tps_view.php
@@ -1,16 +1,67 @@
             <!-- [ Main Content ] start -->
             <div class="row">
         
-                <div class="col-md-12">
+                <div class="col-md-12 col-xl-6">
                     <div class="card">
                         <div class="card-header">
-                            <h5>Data </h5>
+                            <h5>No TPS (<i class="feather icon-map-pin"></i> Alamat)</h5>
+                            <div class="card-header-left">
+                            </div>
+                            <div class="card-header-right">
+                                <i class="icofont icofont-spinner-alt-5"></i>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <p>This is body.</p>
+                        <div class="card-body w-100">
+                            <span>Suara Masuk</span>
+                            <h2 class="f-40 m-b-20">38.1% </h2>
+                            <div class="card-progress p-t-10">
+                                <div class="row ">
+                                    <div class="col-sm-2 text-right ">
+                                        <label class="text-muted ">March</label>
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <div class="progress">
+                                            <div class="progress-bar" style="width:50.3%; background-color: #7267EF"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <label class="text-muted ">50.3%</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-progress p-t-10">
+                                <div class="row">
+                                    <div class="col-sm-2 text-right">
+                                        <label class="text-muted">February</label>
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-danger" style="width:42.7%"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <label class="text-muted">42.7%</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-progress p-t-10">
+                                <div class="row">
+                                    <div class="col-sm-2 text-right">
+                                        <label class="text-muted">January</label>
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-warning" style="width:78.4%"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <label class="text-muted">78.4%</label>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- progressbar static data end -->
+
             </div>
             <!-- [ Main Content ] end -->
