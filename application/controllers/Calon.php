3226c20bb97613d7c9a7fae61a9d8601bb0bb737
Zaid Artur <zaidartur@gmail.com>
2020-08-22 20:03:32 +0700

    Update Calon.php

--- application/controllers/Calon.php
+++ application/controllers/Calon.php
@@ -14,6 +14,12 @@ class Calon extends CI_Controller {
 		
 	}
 
+	public function detail($calon)
+	{
+		$res = $this->m_calon->getById($calon);
+		echo json_encode($res);
+	}
+
 	public function create($id)
 	{
 		$obj 	= $this->m_calon->created($id);
@@ -27,6 +33,35 @@ class Calon extends CI_Controller {
 			$this->session->set_flashdata('error', 'Gagal menyimpan data');
 			header('Location: '.base_url().'paslon/'.$paslon.'_'.$id);
 			// $res = array('response' => 'fail');
+		}
+
+		// echo json_encode($res);
+	}
+
+	public function update($id)
+	{
+		$obj 	= $this->m_calon->update();
+		$paslon = md5('paslon');
+
+		if($obj) {
+			// $res = array('response' => 'sukses');
+			$this->session->set_flashdata('update', 'Berhasil menyimpan data');
+			header('Location: '.base_url().'paslon/'.$paslon.'_'.$id);
+		} else {
+			$this->session->set_flashdata('error', 'Gagal menyimpan data');
+			header('Location: '.base_url().'paslon/'.$paslon.'_'.$id);
+			// $res = array('response' => 'fail');
+		}
+	}
+
+	public function delete($calon)
+	{
+		$obj = $this->m_calon->delete($calon);
+
+		if($obj) {
+			$res = array('response' => 'sukses');
+		} else {
+			$res = array('response' => 'error');
 		}
 
 		echo json_encode($res);
