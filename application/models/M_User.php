0c78d039f4c40ceee3bcbdc4bef2914ca2ac30cb
Zaid Artur <zaidartur@gmail.com>
2020-08-24 10:15:40 +0700

    Update M_User.php

--- application/models/M_User.php
+++ application/models/M_User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_User extends CI_Model {
+class M_user extends CI_Model {
 
 	private $_user  = 'user';
 	private $_tps   = 'hasil_tps';
