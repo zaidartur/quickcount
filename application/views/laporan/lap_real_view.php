32a71ddaf0ec443aea520292d8c1eed2eba6b8cc
Zaid Artur <zaidartur@gmail.com>
2020-08-26 12:45:26 +0700

    Update lap_real_view.php

--- application/views/laporan/lap_real_view.php
+++ application/views/laporan/lap_real_view.php
@@ -1,16 +1,124 @@
             <!-- [ Main Content ] start -->
             <div class="row">
         
-                <div class="col-md-12">
+                <div class="col-md-12 col-sm-12">
                     <div class="card">
                         <div class="card-header">
-                            <h5>Data </h5>
+                            <h5>Data Akhir</h5>
                         </div>
                         <div class="card-body">
-                            <p>This is body.</p>
+                            <?php 
+                                $terhitung = count($tps) - count($data_akhir);
+                                foreach ($tps as $t => $p) {
+                                    $dpt[$t]    = intval($p->dpt_tps);
+                                }
+
+                                foreach ($data_akhir as $d => $a) {
+                                    $sah2[$d]    = intval($a->suara_sah);
+                                    $rusak2[$d]  = intval($a->suara_tidak_sah);
+                                    $kosong2[$d] = intval($a->suara_golput);
+                                    $dpt2[$d]    = intval($a->dpt_tps);
+                                }
+                                $suara_masuk     = array_sum($sah2) + array_sum($rusak2) + array_sum($kosong2);
+                            ?>
+                            <div class="row">
+                                <div class="col-md-2 col-sm-2" style="text-align: center;">
+                                    <p class="text-muted">TPS Selesai</p>
+                                    <h3 class="f-w-400"><?php echo count($data_akhir).' dari '.count($tps); ?></h3>
+                                </div>
+                                <div class="col-md-8 col-sm-8" style="text-align: center;">
+                                    <h5>
+                                        Total Suara Masuk 
+                                        <span class="badge badge-success"><?=$suara_masuk?></span> 
+                                        dari 
+                                        <span class="badge badge-success"><?=array_sum($dpt)?></span> 
+                                        Jumlah DPT
+                                    </h5>
+                                    <hr class="wid-80 b-wid-3 my-4" style="width: 100%">
+                                    <div class="row">
+                                        <div class="col-md-4 col-sm-4">
+                                            <p class="text-muted">Suara Sah</p>
+                                            <h3 class="f-w-400"><?php echo array_sum($sah2); ?></h3>      
+                                        </div>
+                                        <div class="col-md-4 col-sm-4">
+                                            <p class="text-muted">Suara Tidak Sah / Rusak</p>
+                                            <h3 class="f-w-400"><?php echo array_sum($rusak2); ?></h3>      
+                                        </div>
+                                        <div class="col-md-4 col-sm-4">
+                                            <p class="text-muted">Suara Kosong / Golput</p>
+                                            <h3 class="f-w-400"><?php echo array_sum($kosong2); ?></h3>      
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-2 col-sm-2" style="text-align: center;">
+                                    <p class="text-muted">Tidak Ikut Memilih</p>
+                                    <h3 class="f-w-400"><?php echo array_sum($dpt2) - $suara_masuk; ?></h3>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- progressbar static data end -->
+                <!-- header -->
+
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>Detail TPS</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="dt-responsive table-responsive">
+                                <table id="data-akhir" class="table table-striped table-bordered nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th width="3">#</th>
+                                            <th width="10">No TPS</th>
+                                            <th>Alamat</th>
+                                            <th>Relawan</th>
+                                            <th width="10">Jumlah DPT</th>
+                                            <th width="10">Suara Masuk</th>
+                                            <th class="bg-info" width="10">Suara Sah</th>
+                                            <th class="bg-info" width="10">Suara Rusak</th>
+                                            <th class="bg-info" width="10">Suara Kosong</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            foreach ($data_akhir as $x => $y) { 
+                                                $masuk = intval($y->suara_sah) + intval($y->suara_tidak_sah) + intval($y->suara_golput);
+                                        ?>
+                                        <tr>
+                                            <td><?=$x+1?></td>
+                                            <td><?=$y->no_tps?></td>
+                                            <td><?=$y->alamat_tps?></td>
+                                            <td><?=$y->nama_user?></td>
+                                            <td><?=$y->dpt_tps?></td>
+                                            <td><?=$masuk?></td>
+                                            <td class="bg-info"><?=$y->suara_sah?></td>
+                                            <td class="bg-info"><?=$y->suara_tidak_sah?></td>
+                                            <td class="bg-info"><?=$y->suara_golput?></td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>#</th>
+                                            <th colspan="3">Jumlah</th>
+                                            <th><?php echo array_sum($dpt2); ?></th>
+                                            <th><?php echo $suara_masuk; ?></th>
+                                            <th class="bg-info"><?php echo array_sum($sah2); ?></th>
+                                            <th class="bg-info"><?php echo array_sum($rusak2); ?></th>
+                                            <th class="bg-info"><?php echo array_sum($kosong2); ?></th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <label>&nbsp;</label>
+                            <div class="alert alert-info" role="alert">
+                                Suara Masuk adalah akumulasi dari Suara Sah, Suara Rusak, dan Suara Kosong.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <!-- [ Main Content ] end -->
