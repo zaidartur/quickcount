906b29b5777b5c09f38eaf0832f680b7fdf26b97
Zaid Artur <zaidartur@gmail.com>
2020-09-02 10:15:05 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,6 +54,7 @@ $paslon  = md5('paslon');
 $rel 	 = md5('relawan');
 $tps 	 = md5('tps');
 $lap 	 = md5('laporan');
+$akhir   = md5('akhir');
 $profile = md5('profile');
 
 //login
@@ -78,7 +79,9 @@ $route['paslon/'.$paslon.'_(:any)']    = 'menu/calon/$1';
 $route['relawan/'.$rel.'_(:any)']	   = 'menu/user/$1';
 $route['laporan-tps/'.$tps.'_(:any)']  = 'menu/tps/$1';
 $route['laporan-real/'.$lap.'_(:any)'] = 'menu/laporan/$1';
+$route['laporan-akhir/'.$akhir.'_(:any)']  = 'menu/lapAkhir/$1';
 $route['profile/'.$profile.'_(:any)']  = 'menu/profile/$1';
+
 
 $route['404_override'] = 'menu/notfound';
 $route['translate_uri_dashes'] = FALSE;
