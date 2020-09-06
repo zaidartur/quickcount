fa8a2fc91e3179636d9c2e3e08b4d71b455d073b
Zaid Artur <zaidartur@gmail.com>
2020-09-06 14:11:00 +0700

    Update M_user.php

--- application/models/M_user.php
+++ application/models/M_user.php
@@ -9,6 +9,12 @@ class M_user extends CI_Model {
 	private $_live  = 'live_voting';
 	private $_csatu = 'data_csatu';
 
+	//datatable
+	var $column_order   = array(null, 'nama_user', 'alamat_user', 'email_user', 'username', 'password', 'no_tps', 'alamat_tps', 'dpt_tps');
+	var $column_search  = array('nama_user', 'alamat_user', 'username', 'no_tps', 'alamat_tps', 'dpt_tps');
+	var $order 			= array('nama_user' => 'asc');
+
+
 	public function getAll()
 	{
 		return $this->db->get($this->_user)->result();
@@ -231,6 +237,58 @@ class M_user extends CI_Model {
 	public function validateUsers($user)
 	{
 		return $this->db->get_where($this->_user, array('username' => $user))->result();
+	}
+
+	private function get_query()
+	{
+		$this->db->from($this->_user);
+
+		$i = 0;
+
+		foreach ($this->column_search as $item) {
+			if ($_POST['search']['value']) {
+				if ($i === 0) {
+					$this->db->group_start();
+					$this->db->like($item, $_POST['search']['value']);
+				} else {
+					$this->db->or_like($item, $_POST['search']['value']);
+				}
+
+				if (count($this->column_search) - 1 == $i) 
+					$this->db->group_end();
+			} 
+
+			$i++;
+		}
+
+		if (isset($_POST['order'])) {
+			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+		} else if (isset($this->order)) {
+			$order = $this->order;
+			$this->db->order_by(key($order), $order[key($order)]);
+		}
+	}
+
+	function get_datatables()
+	{
+		$this->get_query();
+		if ($_POST['length'] != -1)
+			$this->db->limit($_POST['length'], $_POST['start']);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function count_filtered()
+	{
+		$this->get_query();
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+
+	public function count_all()
+	{
+		$this->db->from($this->_user);
+		return $this->db->count_all_results();
 	}
 	
 
